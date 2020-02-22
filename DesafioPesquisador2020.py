e9c6aa950910562c138c71b569206fadf5e72577
MohammadNimer <mohammadazul@hotmail.com>
2020-02-22 19:44:37 -0300

    Lendo arquivo csv com pandas

--- DesafioPesquisador2020.py
+++ DesafioPesquisador2020.py
@@ -1,13 +1,5 @@
-import csv
+import pandas as pd
 
-with open('sms_senior.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        print(f'\t{row["Full_Text"]} works in the {row["IsSpam"]} IsSpam, and was born in {row["Word_Count"]}.')
-        line_count += 1
-    print(f'Processed {line_count} lines.')
-    
+df = pd.read_csv('sms_senior.csv', encoding='unicode_escape', index_col='Full_Text', parse_dates=['Date'])
+print(df)
+
