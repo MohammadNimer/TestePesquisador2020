3d57f83dfdb5eba77ce48c5cf84fad8ad7410c9f
MohammadNimer <mohammadazul@hotmail.com>
2020-02-23 22:01:45 -0300

    Realizado as tarefas da primeira etapa

--- DesafioPesquisador2020.py
+++ DesafioPesquisador2020.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 from pylab import *
 
 df = pd.read_csv('sms_senior.csv', encoding='unicode_escape', parse_dates=['Date'])
@@ -28,36 +27,62 @@ pos = arange(len(listName)) + .5
 #show()
 
 ##2 - Agrupar por ano e mes
-dados = df.loc[:,"Word_Count":]
+dados = df.loc[:,"Common_Word_Count":]
 
-dp = pd.DataFrame(data=dados)
+group = pd.DataFrame(data=dados)
 
-dp['Date'] = dp['Date'].astype('datetime64')
+group['Date'] = group['Date'].astype('datetime64')
 
-dp = dp.sort_values(['Date'])
+group['mesAno'] = group['Date'].map(lambda x: 100*x.year + x.month)
+group['IsSpam'] = group['IsSpam'].map({'no':0, 'yes':1})
+group['IsNotSpam'] = group['IsSpam'].map(lambda x: x==0 ).astype('int')
 
-dp['mesAno'] = dp['Date'].map(lambda x: 100*x.year + x.month)
-dp['IsSpam'] = dp['IsSpam'].map({'no':0, 'yes':1})
-dp['IsNotSpam'] = dp['IsSpam'].map(lambda x: x==0 ).astype('int')
+group = group.groupby(['mesAno']).sum().reset_index()
 
-dp = dp.groupby(['mesAno']).sum().reset_index()
+groupFiltered = group.loc[:, group.columns != 'Date']
+groupFiltered = group.loc[:, group.columns != 'Word_Count']
+groupFiltered = group.loc[:, group.columns != 'Common_Word_Count']
 
-dp1 = dp.loc[:, dp.columns != 'Date']
-dp1 = dp.loc[:, dp.columns != 'Word_Count']
-
-#dp1.plot(x = 'mesAno', y = ['IsSpam','IsNotSpam'], kind = 'bar')
+groupFiltered.plot(x = 'mesAno', y = ['IsSpam','IsNotSpam'], kind = 'bar')
 #plt.show()
 
-##3 -
-print('Valor máximo do Word_Count: ' + str(max(dp['Word_Count'])))
-print('Valor minímo do Word_Count: ' + str(min(dp['Word_Count'])))
-##print('Média do Word_Count no mês de Janeiro: ' + str(dp['Word_Count'][0]/3))
-##print('Mediana do Word_Count no mês de Janeiro: ' + str(dp['Word_Count'][0]/3))
-##print('Desvio Padrão do Word_Count no mês de Janeiro: ' + str(dp['Word_Count'][0]/3))
-##print('Variância do Word_Count no mês de Janeiro: ' + str(dp['Word_Count'][0]/3))
+print('########## --------------------------------- ##########')
+##3 - Calculos estatisticos
 
+de = pd.DataFrame(data=dados)
+
+de['Date'] = de['Date'].astype('datetime64')
+de['Date'] = pd.to_datetime(df['Date']).dt.normalize()
+
+de['mesAno'] = de['Date'].map(lambda x: 100*x.year + x.month)
+de = de.loc[:, de.columns != 'IsSpam']
+
+deFilterMax = de['Word_Count'].groupby(de['mesAno']).max()
+deFilterMin = de['Word_Count'].groupby(de['mesAno']).min()
+deFilterMean = de['Word_Count'].groupby(de['mesAno']).mean()
+deFilterMedian = de['Word_Count'].groupby(de['mesAno']).median()
+deFilterStd = de['Word_Count'].groupby(de['mesAno']).std()
+deFilterVar = de['Word_Count'].groupby(de['mesAno']).var()
+
+print('Valor máximo do Word_Count: ' + str(deFilterMax))
+print('Valor minímo do Word_Count: ' + str(deFilterMin))
+print('Média do Word_Count por mês: '+ str(deFilterMean))
+print('Mediana do Word_Count por mês: '+ str(deFilterMedian))
+print('Desvio Padrão do Word_Count por mês: '+ str(deFilterStd))
+print('Variância do Word_Count por mês: '+ str(deFilterVar))
+
+print('########## --------------------------------- ##########')
 ##4 - dia de cada mes que possui a maior sequencia de mensagens comuns/não spam
+dg = pd.DataFrame(data=dados)
 
+dg = dg.loc[:, dg.columns != 'IsNotSpam']
+dg = dg.loc[:, dg.columns != 'Word_Count']
+dg = dg[dg.IsSpam != 1]
+
+dg = dg[['Common_Word_Count','Date']].groupby(dg['mesAno']).max()
+
+print('Maior Sequência de palavras comuns de textos que não são spams por mês: ')
+print(dg)
 
 #Segunda Etapa
 ##Classificar as mensagens por algoritmo proprio
